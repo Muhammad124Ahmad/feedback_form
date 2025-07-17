44ee31dd80b8e47b7ef8760a7312613c592757ba
muhammad1249ahmad <>
2025-07-18 01:38:09 +0500

    Created form, Added Controlled functionality and submission functionality.

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -2,16 +2,48 @@ import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
-
+  const [formData,setFormData]=useState({
+    name:'',
+    email:'',
+    feedback:''
+  });
+  const handleChange=(event)=>{
+    const{name,value}=event.target;
+    setFormData({
+      ...formData,
+      [name]:value
+    });
+  }
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+   const confirmationMessage = `
+      Name: ${formData.name}
+      Email: ${formData.email}
+      Feedback: ${formData.feedback}
+    `;
+    const isConfirmed=window.confirm(`Please Confirm your details :\n\n ${confirmationMessage}`);
+    if(isConfirmed){
+      console.log("Submiting feedback",formData);
+      setFormData({
+        name:'',
+        email:'',
+        feedback:''
+      });
+      alert('Thank you for your valuable feedback!');
+    }
+  }
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form  onSubmit={handleSubmit} className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input type="text" name='name' placeholder='Your Name' onChange={handleChange} />
+        <input type="email" name='email' placeholder='Your Email' onChange={handleChange}/>
+        <textarea name="feedback" placeholder='Your Feedback' onChange={handleChange}></textarea>
+        <button  type='submit'>Submit Feedback</button>
       </form>
     </>
   );
